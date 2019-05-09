4d19c9d90864200ddc9b9dc8eefabd1b4897462f
Jayoung17 <young_1_5@naver.com>
2019-05-09 13:34:50 +0900

    BoardFrontController 두번째 수정

--- Entertain/src/com/controller/BoardFrontController.java
+++ Entertain/src/com/controller/BoardFrontController.java
@@ -1,4 +1,4 @@
-/*package com.controller;
+package com.controller;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
@@ -26,63 +26,11 @@ public class BoardFrontController extends HttpServlet {
 		
 		BoardCommand command=null;
 		String nextPage=null;
-		if(com.equals("/list.do")) {
+		/*if(com.equals("/list.do")) {
 			command=new BoardListCommand();
 			command.execute(request, response);
 			nextPage="list.jsp";
-	}
-	RequestDispatcher dis=request.getRequestDispatcher(nextPage);
-	dis.forward(request,response);
-}
-}*/
-package com.controller;
-
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.service.BoardCommand;
-import com.service.BoardDeleteCommand;
-import com.service.BoardListCommand;
-import com.service.BoardPageCommand;
-import com.service.BoardReplyCommand;
-import com.service.BoardReplyUICommand;
-import com.service.BoardRetrieveCommand;
-import com.service.BoardSearchCommand;
-import com.service.BoardUpdateCommand;
-import com.service.BoardWriteCommand;
-
-
-@WebServlet("*.do")
-public class BoardFrontController extends HttpServlet {
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		/*request.setCharacterEncoding("EUC-KR");*/
-		
-		String requestURI = request.getRequestURI();
-		String contextPath = request.getContextPath();
-		String com = requestURI.substring(contextPath.length());
-
-		
-		BoardCommand command  = null;
-		String nextPage = null;
-		
-		// 목록보기
-		if(com.equals("/list.do")){
-			command = new BoardListCommand();
-			command.execute(request, response);
-			nextPage = "list.jsp";
-		}
+		}*/
 		// 글쓰기 폼
 		if(com.equals("/writeui.do")){
 			nextPage = "write.jsp";
@@ -142,12 +90,7 @@ public class BoardFrontController extends HttpServlet {
 			nextPage = "listPage.jsp";
 		}	
 		
-		
-		RequestDispatcher dis = request.getRequestDispatcher(nextPage);
-		dis.forward(request, response);
-
-	}//
-
+		RequestDispatcher dis=request.getRequestDispatcher(nextPage);
+		dis.forward(request,response);
+	}
 }
-
-
