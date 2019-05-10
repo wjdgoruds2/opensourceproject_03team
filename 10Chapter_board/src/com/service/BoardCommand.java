d8097ac2d8ec2727fd1ed4d1880a3636f44e34a1
kyung <wjdgoruds2@nate.com>
2019-05-10 15:20:12 +0900

    삭제

--- 10Chapter_board/src/com/service/BoardCommand.java
+++
@@ -1,9 +0,0 @@
-package com.service;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface BoardCommand {   
-   //business logic을 구현하기 위한 execute 메서드를 정의한다.
-   public void execute(HttpServletRequest request, HttpServletResponse response);   
-}
